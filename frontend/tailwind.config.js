da31f98615b4bf6291f4abdc407e97819ce7963f
amorgrace <akanforte@gmail.com>
2025-01-23 23:29:40 +0100

    ogogoro

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html', './login.html', './register.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Include all relevant files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+  
